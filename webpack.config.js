be9914f0e37911f0ef48b8d1199c1e03bc0dc7de
Aloxan12 <kvakvaaak@mail.ru>
2023-06-27 13:16:46 +0300

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     mode: "development",
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, 'build')
-    }
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, 'build'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ]
 }
