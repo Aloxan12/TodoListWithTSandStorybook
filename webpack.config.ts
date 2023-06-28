9d5332053f85d2ef06a89432df42e0389cb347fe
Aloxan12 <kvakvaaak@mail.ru>
2023-06-28 15:18:13 +0300

    webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,20 +4,18 @@ import webpack from 'webpack';
 import {buildPlugins} from "./config/build/buildPlugins";
 import {buildLoaders} from "./config/build/buildLoaders";
 import {buildResolvers} from "./config/build/buildResolvers";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPath} from "./config/build/types/config";
 
-const config: webpack.Configuration = {
-    mode: "development",
+const paths: BuildPath = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, 'build'),
-        clean: true
-    },
-    plugins: buildPlugins(),
-    module: {
-        rules: buildLoaders(),
-    },
-    resolve: buildResolvers(),
+    build: path.resolve(__dirname, 'build'),
+    html:path.resolve(__dirname, 'public', 'index.html')
 }
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths
+})
 
 export default config
