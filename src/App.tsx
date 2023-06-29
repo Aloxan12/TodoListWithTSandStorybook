32370edbe53e0a8dae3abf11dcb41d52706e27bb
Aloxan12 <kvakvaaak@mail.ru>
2023-06-29 15:09:37 +0300

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
 import {Counter} from "./components/Counter";
 import './index.scss'
+import {AboutAsync} from "./pages/About/About.async";
+import {MainAsync} from "./pages/Main/Main.async";
 
 export const App = () => {
     return (
         <div className='app'>
-            ккцуц
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>...Loading</div>}>
+                <Routes>
+                    <Route path='/about' element={<AboutAsync />}/>
+                    <Route path='/' element={<MainAsync />}/>
+                </Routes>
+            </Suspense>
             <Counter />
         </div>
     );
