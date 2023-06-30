24e88b49a62690ba90b9248154133de87a716d13
Aloxan12 <kvakvaaak@mail.ru>
2023-06-30 09:11:50 +0300

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import { Routes, Route, Link } from 'react-router-dom'
 import {Counter} from "./components/Counter";
 import './styles/index.scss'
 import {AboutAsync} from "./pages/About/About.async";
 import {MainAsync} from "./pages/Main/Main.async";
 
+export enum Theme {
+    light = 'light',
+    dark = 'dark'
+}
+
 export const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.light)
+
+    const toggleTheme = () =>{
+        setTheme(theme === Theme.dark ? Theme.light : Theme.dark)
+    }
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Поменять тему</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>...Loading</div>}>
