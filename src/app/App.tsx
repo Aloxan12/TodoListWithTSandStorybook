f93173a83134ff9e3ac98eafa9262bc2ffa9e907
Aloxan12 <kvakvaaak@mail.ru>
2023-07-02 10:38:16 +0300

    navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "helpers/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Поменять тему</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
         </div>
     );
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {classNames} from "helpers/classNames/classNames";
+import cls from './Navbar.module.scss'
+interface NavbarProps {
+    className?: string
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+        </div>
+    );
+};
+
+export default Navbar;
