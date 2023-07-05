7b1980bfa1817977e67bc0f1c516229acd03b5d9
Aloxan12 <kvakvaaak@mail.ru>
2023-07-05 13:40:43 +0300

    Sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -0,0 +1,19 @@
+import {classNames} from "helpers/classNames/classNames";
+import cls from './Sidebar.module.scss'
+import {useState} from "react";
+interface SidebarProps {
+    className?: string
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false)
+
+    const onToggle =()=>{
+        setCollapsed(prevState => !prevState)
+    }
+
+    return (
+        <div className={classNames(cls.sidebar, {[cls.collapsed]: collapsed}, [className])}>
+        </div>
+    );
+};
