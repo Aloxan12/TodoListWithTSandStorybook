9569237a804b0093ff6635dfbc3653711bebced9
Aloxan12 <kvakvaaak@mail.ru>
2023-07-06 09:39:13 +0300

    i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "helpers/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar/ui/Sidebar";
+import { useTranslation } from 'react-i18next';
+
+const Component = ()=>{
+    const { t, i18n } = useTranslation();
+    const onToggle = ()=>{
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+    return <div>
+        <button onClick={onToggle}> {t('Перевод')}</button>
+        <span>
+            {t('Текстовый перевод')}
+        </span>
+    </div>
+}
 
 export const App = () => {
     const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback=''>
+                <Component />
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 }
