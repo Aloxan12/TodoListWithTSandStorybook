aa4fd2687149269c0c9bfe6fd9710492da52a082
Aloxan12 <kvakvaaak@mail.ru>
2023-07-06 10:30:48 +0300

    i18n

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,15 +1,21 @@
 import {classNames} from "helpers/classNames/classNames";
 import cls from './LangSwitcher.module.scss'
-import {AppButton} from "shared/ui/AppButton/AppButton";
+import {AppButton, ThemeButton} from "shared/ui/AppButton/AppButton";
 import {useTranslation} from "react-i18next";
+
 interface LangSwitcherProps {
     className?: string
 }
 
 export const LangSwitcher = ({className}: LangSwitcherProps) => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+
+    const onToggle = ()=>{
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
     return (
-        <AppButton className={classNames(cls.langSwitcher, {}, [className])}>
+        <AppButton className={classNames(cls.langSwitcher, {}, [className])} onClick={onToggle} theme={ThemeButton.clear}>
             {t('Язык')}
         </AppButton>
     );
