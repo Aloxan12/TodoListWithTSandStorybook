1211f7776644217a50d3c4caa3448950aa15a86b
Aloxan12 <kvakvaaak@mail.ru>
2023-07-08 10:55:54 +0300

    stylelint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import {classNames} from "helpers/classNames/classNames";
-import cls from './Navbar.module.scss'
-import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import { classNames } from 'helpers/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({className}: NavbarProps) => {
-    return (
-        <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.secondary} className={cls.mainLink} to={'/'}>Главная</AppLink>
-                <AppLink theme={AppLinkTheme.secondary} to={'/about'}>О сайте</AppLink>
-            </div>
+export const Navbar = ({ className }: NavbarProps) => (
+    <div className={classNames(cls.navbar, {}, [className])}>
+        <div className={cls.links}>
+            <AppLink theme={AppLinkTheme.secondary} className={cls.mainLink} to="/">Главная</AppLink>
+            <AppLink theme={AppLinkTheme.secondary} to="/about">О сайте</AppLink>
         </div>
-    );
-};
+    </div>
+);
