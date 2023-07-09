5f10beae46604078d46634180ced52cc18576fbd
Aloxan12 <kvakvaaak@mail.ru>
2023-07-09 09:29:47 +0300

    stylelint

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,21 +1,20 @@
-import {classNames} from "helpers/classNames/classNames";
-import cls from './LangSwitcher.module.scss'
-import {AppButton, ThemeButton} from "shared/ui/AppButton/AppButton";
-import {useTranslation} from "react-i18next";
+import { classNames } from 'helpers/classNames/classNames';
+import { AppButton, ThemeButton } from 'shared/ui/AppButton/AppButton';
+import { useTranslation } from 'react-i18next';
 
 interface LangSwitcherProps {
     className?: string
 }
 
-export const LangSwitcher = ({className}: LangSwitcherProps) => {
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
     const { t, i18n } = useTranslation();
 
-    const onToggle = ()=>{
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru').catch((err)=> console.log(err))
-    }
+    const onToggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru').catch((err) => console.log(err));
+    };
 
     return (
-        <AppButton className={classNames(cls.langSwitcher, {}, [className])} onClick={onToggle} theme={ThemeButton.clear}>
+        <AppButton className={classNames('', {}, [className])} onClick={onToggle} theme={ThemeButton.clear}>
             {t('Язык')}
         </AppButton>
     );
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,6 @@ import { classNames } from 'helpers/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { AppButton, ThemeButton } from 'shared/ui/AppButton/AppButton';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
     className?: string
@@ -14,7 +13,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     return (
         <AppButton
             theme={ThemeButton.clear}
-            className={classNames(cls.themeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
             {theme === Theme.dark ? <DarkIcon /> : <LightIcon />}
