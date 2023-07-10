d5de3d07a169b15f50ddeb264a19ac2c1a85bc39
Aloxan12 <kvakvaaak@mail.ru>
2023-07-10 12:43:43 +0300

    page not found

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,27 +1,24 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routerConfig/routerConfig';
-import { useTranslation } from 'react-i18next';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
-export const AppRouter = () => {
-  const { t } = useTranslation();
-  return (
-    <Suspense fallback={<div>{t('Загрузка')}</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={(
-              <div
-                className="page-wrapper"
-              >
-                {element}
-              </div>
-            )}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+export const AppRouter = () => (
+  <Suspense fallback={<PageLoader />}>
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={(
+            <div
+              className="page-wrapper"
+            >
+              {element}
+            </div>
+          )}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
---
+++ src/widgets/PageLoader/PageLoader.tsx
@@ -0,0 +1,21 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import './PageLoader.scss';
+
+interface PageLoaderProps {
+    className?: string
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => (
+  <div className={classNames('page__loader', {}, [className])}>
+    <div className="lds-roller">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+);
