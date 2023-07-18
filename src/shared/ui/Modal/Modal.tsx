98be83ae8fa1d5dea80b0c5d4d3dafca9efcee03
Aloxan12 <kvakvaaak@mail.ru>
2023-07-18 15:01:31 +0300

    modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,18 +1,34 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
     className?: string
     children?: ReactNode
+    isOpen?: boolean
+    onClose?: () => void
 }
 
-export const Modal = ({ className, children }: ModalProps) => (
-    <div className={classNames(cls.modal, {}, [className])}>
-        <div className={cls.overlay}>
-            <div className={cls.open}>
-                {children}
+export const Modal = ({
+    className, children, isOpen, onClose,
+}: ModalProps) => {
+    const onCloseHandler = () => {
+        if (onClose) onClose();
+    };
+    const onContentClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    const mods: Record<string, boolean> = {
+        [cls.opened]: isOpen,
+    };
+    return (
+        <div className={classNames(cls.modal, mods, [className])}>
+            <div className={cls.overlay} onClick={onCloseHandler}>
+                <div className={cls.content} onClick={onContentClick}>
+                    {children}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
