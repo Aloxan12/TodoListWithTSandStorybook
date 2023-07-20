b963aa0b7e28e6946d1c80a7108c8210141f4427
Aloxan12 <kvakvaaak@mail.ru>
2023-07-20 13:52:38 +0300

    Input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,14 +1,45 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
+import React, { InputHTMLAttributes, memo } from 'react';
 import cls from './Input.module.scss';
 
-interface InputProps {
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+
+interface InputProps extends HTMLInputProps {
     className?: string
+    value?: string
+    onChange?: (value: string) => void
 }
 
-export const Input = ({ className }: InputProps) => {
-    const { t } = useTranslation();
+export const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        type = 'text',
+        placeholder,
+        ...otherProps
+    } = props;
+
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e.target.value);
+    };
+
     return (
-        <div className={classNames(cls.input, {}, [className])} />
+        <div className={classNames(cls.inputWrapper, {}, [className])}>
+            {placeholder && (
+                <div className={cls.placeholder}>
+                    {`${placeholder}>`}
+                </div>
+            )}
+            <div className={cls.caretWrapper}>
+                <input
+                    type={type}
+                    value={value}
+                    onChange={onChangeHandler}
+                    className={cls.input}
+                />
+                <span className={cls.caret} />
+            </div>
+        </div>
     );
-};
+});
