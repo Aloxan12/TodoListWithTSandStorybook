0e72bfdca793998393129a53c5760c4e354508bd
Aloxan12 <kvakvaaak@mail.ru>
2023-07-27 11:55:59 +0300

    ProfilePage

---
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+
+interface ProfilePageProps {
+    className?: string
+}
+
+const ProfilePage = ({ className }: ProfilePageProps) => {
+    const { t } = useTranslation();
+    return (
+        <div />
+    );
+};
+
+export default ProfilePage;
