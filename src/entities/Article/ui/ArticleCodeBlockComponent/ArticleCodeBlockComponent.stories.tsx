7131779d3bd0ab09d21b895ee9412e74efbd980c
Aloxan12 <kvakvaaak@mail.ru>
2023-08-08 10:24:15 +0300

    Articles template

---
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { ArticleCodeBlockComponent } from './ArticleCodeBlockComponent';
+
+export default {
+    title: 'shared/ArticleCodeBlockComponent',
+    component: ArticleCodeBlockComponent,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ArticleCodeBlockComponent>;
+
+const Template: ComponentStory<typeof ArticleCodeBlockComponent> = (args) => <ArticleCodeBlockComponent {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Text',
+};
--- src/pages/ArticleDetailPage/ui/ArticleDetailPage/ArticleDetailPage.tsx
+++ src/pages/ArticleDetailPage/ui/ArticleDetailPage/ArticleDetailPage.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import cls from 'src/pages/ArticleDetailPage/ui/ArticleDetailPage/ArticleDetailPage.module.scss';
 import { memo } from 'react';
+import { ArticleDetails } from 'entities/Article';
 
 interface ArticleDetailPageProps {
     className?: string
@@ -11,7 +12,7 @@ const ArticleDetailPage = ({ className }: ArticleDetailPageProps) => {
     const { t } = useTranslation('article');
     return (
         <div className={classNames(cls.ArticleDetailPage, {}, [className])}>
-            {t('Article Detail')}
+            <ArticleDetails />
         </div>
     );
 };
