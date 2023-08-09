9a130f212df341b2c2745d557767f004015dcc4a
Aloxan12 <kvakvaaak@mail.ru>
2023-08-09 16:25:40 +0300

    Skeleton

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -7,6 +7,9 @@ import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/Dynamic
 import { fetchArticleById } from 'entities/Article/model/services/fetchArticleById/fetchArticleById';
 import { useSelector } from 'react-redux';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import CalendarIcon from 'shared/assets/icons/calendar-20-20.svg';
 import cls from './ArticleDetails.module.scss';
 import { articleDetailsReducer } from '../../model/slice/articleDetailsSlice';
 import {
@@ -39,13 +42,13 @@ export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
 
     if (isLoading) {
         content = (
-            <div>
+            <>
                 <Skeleton className={cls.avatar} width={200} height={200} border="50%" />
                 <Skeleton className={cls.title} width={300} height={32} />
                 <Skeleton className={cls.skeleton} width={600} height={24} />
                 <Skeleton className={cls.skeleton} width="100%" height={200} />
                 <Skeleton className={cls.skeleton} width="100%" height={200} />
-            </div>
+            </>
         );
     }
     if (error) {
@@ -59,7 +62,22 @@ export const ArticleDetails = memo(({ className, id }: ArticleDetailsProps) => {
     }
 
     if (!isLoading && !error) {
-        content = <div>{t('test')}</div>;
+        content = (
+            <>
+                <div className={cls.avatarWrapper}>
+                    <Avatar size={200} src={article?.img} />
+                </div>
+                <Text title={article?.title} text={article?.subtitle} />
+                <div className={cls.articleInfo}>
+                    <EyeIcon />
+                    <Text text={String(article?.views)} />
+                </div>
+                <div className={cls.articleInfo}>
+                    <CalendarIcon />
+                    <Text text={article?.createdAt} />
+                </div>
+            </>
+        );
     }
 
     return (
