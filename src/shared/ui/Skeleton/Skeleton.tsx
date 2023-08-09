8050bbe44c9749a9743be233618230a66bd782d8
Aloxan12 <kvakvaaak@mail.ru>
2023-08-09 10:39:41 +0300

    Skeleton

--- src/shared/ui/Skeleton/Skeleton.tsx
+++ src/shared/ui/Skeleton/Skeleton.tsx
@@ -1,15 +1,27 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
+import { CSSProperties, memo } from 'react';
 import cls from './Skeleton.module.scss';
 
 interface SkeletonProps {
     className?: string
+    width?: number | string
+    height?: number | string
+    border?: string
 }
 
-export const Skeleton = memo(({ className }: SkeletonProps) => {
-    const l = '';
+export const Skeleton = memo(({
+    className, height, width, border,
+}: SkeletonProps) => {
+    const style: CSSProperties = {
+        width,
+        height,
+        borderRadius: border,
+    };
     return (
-        <div className={classNames(cls.Skeleton, {}, [className])} />
+        <div
+            className={classNames(cls.Skeleton, {}, [className])}
+            style={style}
+        />
     );
 });
