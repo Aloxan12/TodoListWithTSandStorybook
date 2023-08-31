1025f48798852713b4d44b966c1f8f6c94ef3959
Aloxan12 <kvakvaaak@mail.ru>
2023-08-31 13:24:13 +0300

    add article big item

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,13 +1,17 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Text } from 'shared/ui/Text/Text';
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { Icon } from 'shared/ui/Icon/Icon';
 import Eye from 'shared/assets/icons/eye-20-20.svg';
 import { Card } from 'shared/ui/Card/Card';
 import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './ArticleListItem.module.scss';
-import { Article, ArticleView } from '../../model/types/article';
+import {
+    Article, ArticleBlockType, ArticleTextBlock, ArticleView,
+} from '../../model/types/article';
+import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 
 interface ArticleListItemProps {
     className?: string
@@ -16,7 +20,19 @@ interface ArticleListItemProps {
 }
 
 export const ArticleListItem = memo(({ className, article, view }: ArticleListItemProps) => {
+    const { t } = useTranslation();
+    const types = <Text text={article.type.join(', ')} className={cls.types} />;
+    const views = (
+        <>
+            <Text text={String(article.views)} className={cls.views} />
+            <Icon Svg={Eye} />
+        </>
+    );
+
     if (view === ArticleView.BIG) {
+        const textBlock = article.blocks.find(
+            (block) => block.type === ArticleBlockType.TEXT,
+        ) as ArticleTextBlock;
         return (
             <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
                 <Card className={cls.card}>
@@ -24,6 +40,15 @@ export const ArticleListItem = memo(({ className, article, view }: ArticleListIt
                         <Avatar size={30} src={article.user.avatar} />
                         <Text text={article.user.username} className={cls.username} />
                         <Text text={article.createdAt} className={cls.date} />
+                    </div>
+                    <Text text={article.title} className={cls.title} />
+                    {types}
+                    <img src={article.img} alt={article.title} className={cls.img} />
+                    {textBlock && <ArticleTextBlockComponent block={textBlock} />}
+                    <div className={cls.footer}>
+                        <Button theme={ButtonTheme.OUTLINE}>
+                            {t('Читать далее...')}
+                        </Button>
                     </div>
                 </Card>
             </div>
@@ -37,9 +62,8 @@ export const ArticleListItem = memo(({ className, article, view }: ArticleListIt
                     <Text text={article.createdAt} className={cls.date} />
                 </div>
                 <div className={cls.infoWrapper}>
-                    <Text text={article.type.join(', ')} className={cls.types} />
-                    <Text text={String(article.views)} className={cls.views} />
-                    <Icon Svg={Eye} />
+                    {types}
+                    {views}
                 </div>
                 <Text text={article.title} className={cls.title} />
             </Card>
