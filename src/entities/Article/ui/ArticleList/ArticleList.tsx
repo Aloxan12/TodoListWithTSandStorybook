f5b654967f278353f85c5eb279a248eb90b95c42
Aloxan12 <kvakvaaak@mail.ru>
2023-08-31 14:15:28 +0300

    add article big item

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
+import { ArticleListItemSkeleton } from 'entities/Article/ui/ArticleListItem/ArticleListItemSkeleton';
 import { Article, ArticleView } from '../../model/types/article';
 import cls from './ArticleList.module.scss';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
@@ -11,9 +12,28 @@ interface ArticleListProps {
     view?: ArticleView
 }
 
+const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.SMALL ? 9 : 3)
+    .fill(0)
+    .map((item, index) => (
+        <ArticleListItemSkeleton
+            // eslint-disable-next-line react/no-array-index-key
+            key={`${item}${index}`}
+            className={cls.card}
+            view={view}
+        />
+    ));
+
 export const ArticleList = memo(({
     className, articles, view = ArticleView.SMALL, isLoading,
 }: ArticleListProps) => {
+    if (isLoading) {
+        return (
+            <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+                {getSkeletons(view)}
+            </div>
+        );
+    }
+
     const renderArticle = (article: Article) => (
         <ArticleListItem
             key={article.id}
