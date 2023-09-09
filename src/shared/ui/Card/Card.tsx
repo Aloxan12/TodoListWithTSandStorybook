47292be6788ae54446fb4708373e03d2599e2d70
Aloxan12 <kvakvaaak@mail.ru>
2023-09-09 11:37:31 +0300

    tabs

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -3,13 +3,21 @@ import { useTranslation } from 'react-i18next';
 import { HTMLAttributes, memo, ReactNode } from 'react';
 import cls from './Card.module.scss';
 
+export enum CardTheme {
+    NORMAL = 'normal',
+    OUTLINED = 'outlined',
+}
+
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
     className?: string
     children: ReactNode
+    theme?: CardTheme
 }
 
-export const Card = memo(({ className, children, ...otherProps }: CardProps) => (
-    <div className={classNames(cls.Card, {}, [className])} {...otherProps}>
+export const Card = memo(({
+    className, children, theme = CardTheme.NORMAL, ...otherProps
+}: CardProps) => (
+    <div className={classNames(cls.Card, {}, [className, cls[theme]])} {...otherProps}>
         {children}
     </div>
 ));
