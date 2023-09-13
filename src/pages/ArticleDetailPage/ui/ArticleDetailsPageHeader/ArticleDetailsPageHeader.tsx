16d3eb61261b29c9d5e69a5ce99c6925c509aa39
Aloxan12 <kvakvaaak@mail.ru>
2023-09-13 09:34:35 +0300

    ArticleEditPage

--- src/pages/ArticleDetailPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.tsx
+++ src/pages/ArticleDetailPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.tsx
@@ -5,6 +5,7 @@ import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { getArticleDetailsData } from 'entities/Article';
 import cls from './ArticleDetailsPageHeader.module.scss';
 import { getCanEditArticle } from '../../model/selectors/article';
 
@@ -16,17 +17,23 @@ export const ArticleDetailsPageHeader = memo(({ className }: ArticleDetailsPageH
     const { t } = useTranslation();
     const navigate = useNavigate();
     const isCanEdit = useSelector(getCanEditArticle);
+    const article = useSelector(getArticleDetailsData);
 
     const onBackToList = useCallback(() => {
         navigate(RoutePath.articles);
     }, [navigate]);
+
+    const onEditArticle = useCallback(() => {
+        navigate(`${RoutePath.articles}/${article?.id}/edit`);
+    }, [article?.id, navigate]);
+
     return (
         <div className={classNames(cls.ArticleDetailsPageHeader, {}, [className])}>
             <Button theme={ButtonTheme.OUTLINE} onClick={onBackToList}>
                 {t('Назад к списку')}
             </Button>
             {isCanEdit && (
-                <Button className={cls.editBtn} theme={ButtonTheme.OUTLINE} onClick={onBackToList}>
+                <Button className={cls.editBtn} theme={ButtonTheme.OUTLINE} onClick={onEditArticle}>
                     {t('Редактировать')}
                 </Button>
             )}
