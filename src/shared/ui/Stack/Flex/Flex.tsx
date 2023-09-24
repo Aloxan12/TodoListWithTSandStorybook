8c5388958b8751753c81a4a16db7c181d058a74e
Aloxan12 <kvakvaaak@mail.ru>
2023-09-24 12:17:42 +0300

    flex

--- src/shared/ui/Stack/Flex/Flex.tsx
+++ src/shared/ui/Stack/Flex/Flex.tsx
@@ -3,12 +3,21 @@ import { useTranslation } from 'react-i18next';
 import { memo, ReactNode } from 'react';
 import cls from './Flex.module.scss';
 
+export type FlexJustify = 'start' | 'center' | 'end' | 'between'
+export type FlexAlign = 'start' | 'center' | 'end'
+export type FlexDirection = 'row' | 'column'
+
 interface FlexProps {
     className?: string
     children: ReactNode
+    justify?: FlexJustify
+    align?: FlexAlign
+    direction?: FlexDirection
 }
 
-export const Flex = ({ className, children }: FlexProps) => {
+export const Flex = ({
+    className, children, justify = 'start', align = 'center', direction = 'row',
+}: FlexProps) => {
     const { t } = useTranslation();
     return (
         <div className={classNames(cls.Flex, {}, [className])}>
